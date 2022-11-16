a32a1b1ee24c39b0763461096292c5ddf542bd36
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-11-16 00:00:23 -0300

    Login & register

--- home/views.py
+++ home/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+import os
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.forms.models import model_to_dict
+from django.shortcuts import redirect
+from django.shortcuts import render
 
 
 from accommodation.models import Accommodation
+from home.forms import AvatarForm
+from home.forms import UserRegisterForm
+from home.forms import UserUpdateForm
+from home.models import Avatar
+
+def get_avatar_url_ctx(request):
+    avatars = Avatar.objects.filter(user=request.user.id)
+    if avatars.exists():
+        return {"avatar_url": avatars[0].image.url}
+    return {}
+
 
 def index(request):
     return render(
@@ -28,4 +44,61 @@ def search(request):
         request=request,
         context=context_dict,
         template_name="accommodation/accommodation_list.html",
+    )
+
+def register(request):
+    form = UserRegisterForm(request.POST) if request.POST else UserRegisterForm()
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, "El usuario ha sido creado!")
+            return redirect("login")
+
+    return render(
+        request=request,
+        context={"form": form},
+        template_name="registration/register.html",
+    )
+
+
+@login_required
+def user_update(request):
+    user = request.user
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("home:index")
+
+    form = UserUpdateForm(model_to_dict(user))
+    return render(
+        request=request,
+        context={"form": form},
+        template_name="registration/user_form.html",
+    )
+
+
+@login_required
+def avatar_load(request):
+    if request.method == "POST":
+        form = AvatarForm(request.POST, request.FILES)
+        if form.is_valid and len(request.FILES) != 0:
+            image = request.FILES["image"]
+            avatars = Avatar.objects.filter(user=request.user.id)
+            if not avatars.exists():
+                avatar = Avatar(user=request.user, image=image)
+            else:
+                avatar = avatars[0]
+                if len(avatar.image) > 0:
+                    os.remove(avatar.image.path)
+                avatar.image = image
+            avatar.save()
+            messages.success(request, "Imagen subida con éxito")
+            return redirect("home:index")
+
+    form = AvatarForm()
+    return render(
+        request=request,
+        context={"form": form},
+        template_name="home/avatar_form.html",
     )
--- mochilas_viajeras/settings.py
+++ mochilas_viajeras/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +131,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
