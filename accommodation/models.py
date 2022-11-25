ec4f8f24980b029aec43fd48bbe7779f9ce978b9
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-11-25 10:02:41 -0300

    corrección modelo accommodation

--- accommodation/models.py
+++ accommodation/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 from ckeditor.fields import RichTextField
-from django.forms import FloatField, IntegerField
 
 class Accommodation(models.Model):
     name = models.CharField(max_length=50)
-    location = models.TextField()
+    location = models.TextField(null=True, blank=True)
     description = RichTextField(null=True, blank=True)
-    contact = IntegerField()
-    price = FloatField()
+    contact = models.IntegerField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
