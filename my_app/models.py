fc431a256ea658942a1da18d11706e38d6c3d5b2
Johanna Cornejo <johanna.cornejo@hotmail.com>
2022-12-02 00:40:26 -0300

    se agrego owner a template destino-list

--- my_app/models.py
+++ my_app/models.py
@@ -8,12 +8,9 @@ class Destino(models.Model):
     name = models.CharField(max_length=40)
     year = models.IntegerField()
     description = RichTextField(null=True, blank=True)
-
     image = models.ImageField(upload_to='destino', null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-   
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
 
     def __str__(self):
